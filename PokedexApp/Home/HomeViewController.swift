3fde3e5686e53e39247923b4532f3e14672d73b0
Axl Estevez <axlestevez@gmail.com>
2023-10-18 01:17:53 -0600

    first commit

---
+++ PokedexApp/Home/HomeViewController.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewController.swift
+//  PokedexApp
+//
+//  Created by Axl Estevez on 18/10/23.
+//
+
+import Foundation
---
+++ PokedexApp/SpinnerView/SpinnerView.swift
@@ -0,0 +1,20 @@
+//
+//  SpinnerView.swift
+//  PokedexApp
+//
+//  Created by Axl Estevez on 18/10/23.
+//
+
+import UIKit
+
+class SpinnerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
