d00cad2b9608b2499e87d003db14e69c911279a3
Axl Estevez <axlestevez@gmail.com>
2023-10-25 13:44:52 -0600

    Add source code

--- PokedexApp/Home/HomeViewController.swift
+++ PokedexApp/Home/HomeViewController.swift
@@ -5,4 +5,25 @@
 //  Created by Axl Estevez on 18/10/23.
 //
 
-import Foundation
+import UIKit
+import Lottie
+
+class HomeViewController: UIViewController {
+    
+    private var spinnerView: SpinnerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        showSppiner()
+    }
+    
+    private func showSppiner() {
+        spinnerView = SpinnerView(frame: CGRect(
+            x: 0,
+            y: 0,
+            width: self.view.bounds.width,
+            height: self.view.bounds.height
+        ))
+        self.view.addSubview(spinnerView)
+    }
+}
--- PokedexApp/SpinnerView/SpinnerView.swift
+++ PokedexApp/SpinnerView/SpinnerView.swift
@@ -6,15 +6,41 @@
 //
 
 import UIKit
+import Lottie
 
 class SpinnerView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    @IBOutlet var contentView: UIView!
+    @IBOutlet var spinnerView: UIView!
+    
+    private var animationView: LottieAnimationView?
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+        setupAnimation()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func setupAnimation() {
+        animationView = .init(name: "pokeball")
+        animationView!.frame = spinnerView.bounds
+        animationView!.contentMode = .scaleAspectFit
+        animationView!.loopMode = .loop
+        animationView!.animationSpeed = 0.5
+        spinnerView.addSubview(animationView!)
+        animationView!.play()
+    }
+    
+    private func commonInit() {
+        Bundle.main.loadNibNamed("SpinnerView", owner: self)
+        addSubview(contentView)
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    }
+    
 }
